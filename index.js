390e6f576ab16a2b586bb3e48701187b836e0a6d
Adrish Dey <adrishdey2007@gmail.com>
2026-04-15 18:44:03 +0530

    card added in backend and axios used to get APIs

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const path = require("path");
+const axios = require("axios");
 const {auth, requiresAuth} = require("express-openid-connect");
+const { profile } = require("console");
 require("dotenv").config();
 const port = 3000;
 const app = express();
@@ -40,3 +42,26 @@ app.get("/search",requiresAuth(),(req,res)=>{
     res.render('search', {error : null});
 })
 
+// card-display-page
+
+// recieve the username
+app.get("/card",requiresAuth(),(req,res)=>{
+    const username = req.query.username;
+    const profileURL = `https://api.github.com/users/${username}`;
+    const repoURL = `https://api.github.com/users/${username}/repos`;
+    
+    async function getUser(){
+        try{
+            const info = await axios.get(profileURL);
+            console.log(info.data.login);
+        }catch(err){
+            console.log(err.message);
+            console.log("error!!!");
+        }
+    }
+    getUser();
+    res.send("check terminal for json");
+    console.log(req.query);
+})
+// shortlist the top languages
+// display info
